768a6aaa62be6284fdb9a50c8628f3b6618d4336
Peeter Oliver <peeter@surnuaed.net>
2013-05-14 03:21:44 +0300

    yolo

--- www/js/walter.js
+++ www/js/walter.js
@@ -9,24 +9,21 @@ function ymardamine(n, l)
 				
 				$("#nupp1").click(function()
 				{
+				
+				a = $("#eur").val();
+				b = $("#sent").val();
+				
+				raha = parseInt(a) + (b/100);
 
         			//document.getElementById('b1').value = ymardamine($("#a1").val() / 0.53, 0);
-        			$('#yes').text( ymardamine($("#a1").val() / 0.53, 0) );
-        			$('#yes2').text( $("#a1").val() );
-        			
-              $('.hidn').fadeIn(1);
+        			$('#yes').text( ymardamine(raha / 0.53, 0) );
+        			$('#yes2').text( $("#eur").val() );
+        			$('#yes3').text( $("#sent").val() );
+
               
     			});
     			
-    			$("#slide").click(function()
-				{
-
-        			$("#part1").animate({width:'0px'}, function() {
-    $("#part1").css('display','none');
-  });
-        			$("#part2").animate({width:'320px'});
-              
-    			});
+    			
     			
 
 				
