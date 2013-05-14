ffd3d70ad8ba70054c4819c847655c5e56972bea
Peeter Oliver <peeter@surnuaed.net>
2013-05-14 05:39:49 +0300

    yoloswag

--- www/js/walter.js
+++ www/js/walter.js
@@ -16,7 +16,10 @@ function ymardamine(n, l)
 				raha = parseInt(a) + (b/100);
 
         			//document.getElementById('b1').value = ymardamine($("#a1").val() / 0.53, 0);
-        			$('#yes').text( ymardamine(raha / 0.53, 0) );
+        			$('#r1').text( ymardamine(raha / 0.53, 0) );
+        			$('#r2').text( ymardamine(raha / 0.73, 0) );
+        			$('#r3').text( ymardamine(raha / 1.73, 0) );
+        			$('#r4').text( ymardamine(raha / 2.37, 0) );
         			$('#yes2').text( $("#eur").val() );
         			$('#yes3').text( $("#sent").val() );
 
